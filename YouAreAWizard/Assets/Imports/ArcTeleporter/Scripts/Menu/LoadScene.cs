c0f9ab2e1f83361d338c06025baabf841862c1a3
ocesiwm <oceaneperetti@gmail.com>
2019-11-15 23:34:36 +0000

    fixing level2 final step

--- YouAreAWizard/Assets/Imports/ArcTeleporter/Scripts/Menu/LoadScene.cs
+++ YouAreAWizard/Assets/Imports/ArcTeleporter/Scripts/Menu/LoadScene.cs
@@ -3,23 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LoadScene : MonoBehaviour {
-	UnityEngine.SceneManagement.Scene scene;
+    UnityEngine.SceneManagement.Scene scene;
 
-	void Start () {
-		scene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
-	}
-	
-	void Update () {
-		if (Input.GetButtonDown ("Cancel")) {
-			if (scene.name != "Start") {
-				UnityEngine.SceneManagement.SceneManager.LoadScene ("Start", UnityEngine.SceneManagement.LoadSceneMode.Single);
-			}
-		}
-	}
-
-	public void OnSelect(Transform t) {
-		string name = t.gameObject.name;
-		UnityEngine.SceneManagement.SceneManager.LoadScene (name, UnityEngine.SceneManagement.LoadSceneMode.Single);
-	}
+    void Start() {
+        scene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
+    }
+    public void Attack()
+    {
+        GameManager.instance.AttackLevel();
+    }
+    public void Wave()
+    {
+        GameManager.instance.WaveLevel();
+    }
+	public void Boss()
+    {
+        GameManager.instance.BossLevel();
+    }
 
 }
